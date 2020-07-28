f6aeead0a6613f11b94e441fa7b4cedfe56aa153
Kai-Wen Hu <kainbd0609@gmail.com>
2020-07-28 21:28:24 +0800

    Return errno when function failed
    We should return errno when function is
    failed.

--- src/encfs.cc
+++ src/encfs.cc
@@ -1,24 +1,46 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <cerrno>
+#include <cstring>
+#include <string>
 
 #include "encfs.h"
+#include "logger.h"
 
 #define UNUSED [[maybe_unused]]
 
+using std::string;
+
 int encfs_getattr(const char *fpath, struct stat *statbuf)
 {
-    return lstat(fpath, statbuf);
+    LOGGER->WriteLog("getattr: " + string(fpath));
+    if (0 != lstat(fpath, statbuf)) {
+        return -errno;
+    }
+    return 0;
 }
 
 int encfs_mknod(const char *fpath, mode_t mode, dev_t dev)
 {
-    return mknod(fpath, mode, dev);
+    LOGGER->WriteLog("mknod: " + string(fpath));
+    if (0 != mknod(fpath, mode, dev)) {
+        return -errno;
+    }
+    return 0;
 }
 
 int encfs_open(const char *fpath, struct fuse_file_info *fi)
 {
-    return open(fpath, fi->flags);
+    int fd = -1;
+
+    LOGGER->WriteLog("open: " + string(fpath));
+
+    if (-1 == (fd = open(fpath, fi->flags))) {
+        return -errno;
+    }
+    fi->fh = fd;
+    return 0;
 }
 
 int encfs_read(UNUSED const char *fpath,
@@ -27,7 +49,13 @@ int encfs_read(UNUSED const char *fpath,
                off_t offset,
                struct fuse_file_info *fi)
 {
-    return pread(fi->fh, buf, size, offset);
+    ssize_t ret = -1;
+    LOGGER->WriteLog("read: " + string(fpath));
+
+    if (-1 == (ret = pread(fi->fh, buf, size, offset))) {
+        return -errno;
+    }
+    return ret;
 }
 
 int encfs_write(UNUSED const char *fpath,
@@ -36,5 +64,11 @@ int encfs_write(UNUSED const char *fpath,
                 off_t offset,
                 struct fuse_file_info *fi)
 {
-    return pwrite(fi->fh, buf, size, offset);
+    ssize_t ret = -1;
+    LOGGER->WriteLog("write: " + string(fpath));
+
+    if (-1 == (ret = pwrite(fi->fh, buf, size, offset))) {
+        return -errno;
+    }
+    return ret;
 }
