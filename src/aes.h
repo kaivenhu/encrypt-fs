31ba663b5480213c5c86d1851496be5f07e0f900
Kai-Wen Hu <kainbd0609@gmail.com>
2020-08-23 22:36:06 +0800

    AES: implement 128 bit key expansion

---
+++ src/aes.h
@@ -0,0 +1,51 @@
+#ifndef ENCFS_AES_H_
+#define ENCFS_AES_H_
+#include <array>
+#include <cstdint>
+#include <vector>
+
+namespace encfs
+{
+namespace aes
+{
+class AES
+{
+    static const std::array<uint8_t, 256> S_BOX;
+    static const std::array<uint32_t, 10> RCON;
+
+    int round_;
+
+    std::array<uint32_t, 44> round_key_;
+
+    uint32_t SubWord(const uint32_t &word)
+    {
+        uint32_t x = 0;
+        for (int i = 3; i >= 0; --i) {
+            x |= S_BOX[static_cast<uint8_t>((word >> (8 * i)) & 0xF)]
+                 << (8 * i);
+        }
+        return x;
+    }
+    uint32_t RotWord(const uint32_t &word, const uint32_t &shift)
+    {
+        return (word << shift) | (word >> (-shift & 31));
+    }
+    void KeyExpansion(const std::array<uint32_t, 4> &key);
+
+public:
+    AES(const std::vector<uint8_t> &input);
+    ~AES() = default;
+    AES(const AES &) = delete;
+    AES(AES &&) = delete;
+    AES &operator=(AES) = delete;
+
+    std::vector<uint32_t> round_key()
+    {
+        return std::vector<uint32_t>(round_key_.begin(), round_key_.end());
+    };
+};
+
+}  // namespace aes
+}  // namespace encfs
+
+#endif /* ENCFS_AES_H_ */
